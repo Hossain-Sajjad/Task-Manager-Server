81ddc30bfbfe5849478eedb1e82f938cd03a4aff
Hossain-Sajjad <sunshinems58.com>
2022-07-01 00:44:25 +0600

    project initialized

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.avl0z.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log('uri:  ', uri, 'client', client)
+
+app.get('/', (req, res) => {
+    res.send('Server working correctly');
+})
+
+app.listen(port, () => console.log('server working correctly', port));
